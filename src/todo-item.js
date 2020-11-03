abf62bdff42e3ebaffd98d52412c478b37066ab5
Jayden Dyer <jaydendyer@Jaydens-Air.domain>
2020-11-03 12:08:52 -0700

    checkbox is bring controlled by props and state

--- src/todo-item.js
+++ src/todo-item.js
@@ -3,12 +3,25 @@ import React, { Component } from "react";
 class TodoItem extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      done: this.props.todo.done,
+    };
   }
+
+  toggleDone = () => {
+    console.log("toggleDone");
+    this.setState((prevState) => ({
+      done: !prevState.done,
+    }));
+  };
   render() {
     return (
       <div className="todo-item">
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={this.state.done}
+          onChange={this.toggleDone}
+        />
         <p>{this.props.todo.title}</p>
       </div>
     );
