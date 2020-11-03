d0c530134856a96daeeba27db9b6c646b474995e
Jayden Dyer <jaydendyer@Jaydens-Air.domain>
2020-11-03 10:08:57 -0700

    button and input

--- src/index.js
+++ src/index.js
@@ -3,11 +3,35 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      todo: "",
+    };
   }
+
+  handleChange = (e) => {
+    this.setState({
+      todo: e.target.value,
+    });
+  };
+
+  addTodo = (e) => {
+    e.preventDefault();
+    console.log("addtodo", this.state.todo);
+  };
+
   render() {
     return (
-      <div>
-        <h1>TODO List</h1>
+      <div className="app">
+        <h1>Todo List</h1>
+        <form className="add-todo" onSubmit={this.addTodo}>
+          <input
+            type="text"
+            placeholder="Add Todo"
+            value={this.state.todo}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add</button>
+        </form>
       </div>
     );
   }
